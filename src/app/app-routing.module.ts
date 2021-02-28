6cb391e9028329556649f4127bdcb22f43848dc8
unknown <steve.potts@bowlereggs.co.uk>
2021-02-28 12:00:48 +0000

    basic comps

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './public/login/login.component';
+import { PublicComponent } from './public/public.component';
+import { RegisterComponent } from './public/register/register.component';
+import { SecureComponent } from './secure/secure.component';
+
+const routes: Routes = [
+  {
+    path: '', component: PublicComponent, children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent }
+    ]
+  },
+  { path: '', component: SecureComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/public/public.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PublicComponent } from './public.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent, PublicComponent],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class PublicModule { }
