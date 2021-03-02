05318aafa94cb1b81b5ce00e972605ed71f60107
unknown <steve.potts@bowlereggs.co.uk>
2021-03-02 22:41:02 +0000

    users pre pagination

--- src/app/secure/users/users.component.ts
+++ src/app/secure/users/users.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  users: User[];
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.all().subscribe((res: any) => {
+      this.users = res.data;
+    });
   }
+
+
 
 }
